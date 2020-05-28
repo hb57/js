2e830a4103d1f81bee7ad13eaa7794d59e821227
hb <horia57@yahoo.com>
2020-05-28 12:50:18 +0300

    Add files via upload

---
+++ webp01.js
@@ -0,0 +1,18 @@
+webpok = 'zzz';
+function hasWebP() {
+    /*
+     * detecteaza WEBP capabilitate a browserului
+     * modificat !!
+    */
+      var rv = $.Deferred();
+      var img = new Image();
+      img.onload = function() { rv.resolve(); };
+      img.onerror = function() { rv.reject(); };
+      img.src = "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==";
+      return rv.promise();
+}
+
+hasWebP().then( 
+      function(){ webpok = true;  },  //webp enabled  
+      function(){ webpok = false; }   //webp disabled
+);
