fcf1473e6ff344c2299cbe06ad6a0ebc90588799
horia bunea <horia57@yahoo.com>
2020-05-28 07:37:47 +0300

    Add files via upload

---
+++ webp.js
@@ -0,0 +1,16 @@
+function hasWebP() {
+    /*
+     * detecteaza WEBP capabilitate a browserului
+    */
+      var rv = $.Deferred();
+      var img = new Image();
+      img.onload = function() { rv.resolve(); };
+      img.onerror = function() { rv.reject(); };
+      img.src = "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==";
+      return rv.promise();
+}
+
+hasWebP().then( 
+      function(){ var webpok = true;  },  //webp enabled  
+      function(){ var webpok = false; }   //webp disabled
+);
